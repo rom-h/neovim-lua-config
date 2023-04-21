2fd300ed3aa5ccff25c607785c41aa21e021d6f8
Romain Helou-Lenoir <helouromain@gmail.com>
2023-04-21 14:25:20 -0400

    null-ls add phpcsfixer formatter

--- lua/user/lsp/null-ls.lua
+++ lua/user/lsp/null-ls.lua
@@ -15,6 +15,7 @@ null_ls.setup({
 		formatting.black.with({ extra_args = { "--fast" } }),
 		formatting.stylua,
 		formatting.google_java_format,
+        formatting.phpcsfixer,
 		diagnostics.flake8,
 	},
 })
